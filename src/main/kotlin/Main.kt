f16972731d7d2b21daddb3bdd664ea44ba3f19a4
Ashley Katt <46717680+ashley-katt@users.noreply.github.com>
2023-05-15 14:17:33 -0500

    Tokenizer complete

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,3 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println(tokenize("fn awesome(param:Type):Output{return 2+2;}").list.joinToString(" ") { it.type.toString() })
 }

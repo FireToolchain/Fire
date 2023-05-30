d52ceb5196d11bc5c00d1f1bdb84879a887f1b78
Ashli Katt <sqrtseven7@gmail.com>
2023-05-30 16:37:15 -0500

    blehhh :3

--- src/main/kotlin/dev/ashli/fire/parser/definitions/Enum.kt
+++ src/main/kotlin/dev/ashli/fire/parser/definitions/Enum.kt
@@ -1,7 +1,6 @@
 package dev.ashli.fire.parser.definitions
 
-import dev.ashli.fire.parser.Resource
-import dev.ashli.fire.parser.TopLevelHolder
+import dev.ashli.fire.parser.TopLevel
 import dev.ashli.fire.parser.annotations.Annotation
 import dev.ashli.fire.resources.ResourceLocation
 
@@ -9,5 +8,5 @@ import dev.ashli.fire.resources.ResourceLocation
  * Represents a Fire enum.
  */
 class Enum(location: ResourceLocation, annotations: List<Annotation>) :
-    TopLevelHolder(location, annotations) {
+    TopLevel(location, annotations) {
 }

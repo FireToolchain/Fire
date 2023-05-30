86b883829d77906144ca0a61374942262030d6d7
Ashli Katt <sqrtseven7@gmail.com>
2023-05-29 19:46:58 -0500

    remove

--- src/main/kotlin/tokenizing/Errors.kt
+++
@@ -1,6 +0,0 @@
-package tokenizing
-
-/**
- * Represents an error that occurs during tokenization.
- */
-class SyntaxError(str: String, line: Int, col: Int) : Error("$str\n At: line $line, column $col.")

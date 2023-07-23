413594a73ac74e6d7c32c8b5b4301a90eeb7fb97
akarahdev <118059229+akarahdev@users.noreply.github.com>
2023-07-23 13:43:58 -0400

    finish CLI args

--- src/main/kotlin/dev/ashli/fire/CommandLineArguments.kt
+++ src/main/kotlin/dev/ashli/fire/CommandLineArguments.kt
@@ -4,4 +4,60 @@ package dev.ashli.fire
  * This class represents a list of command line arguments.
  */
 class CommandLineArguments {
+    private val arguments: Array<CLIArg> = arrayOf()
+
+    /**
+     * Check if the CLI has an argument.
+     * @param arg Argument to check if it exists or not.
+     */
+    fun hasArgument(arg: CLIArg): Boolean {
+        return arguments.contains(arg)
+    }
+
+    /**
+     * Append an argument to the current list of arguments.
+     * Does not add anything if the array already contains the argument.
+     * @param arg Argument to append.
+     */
+    fun addArgument(arg: CLIArg) {
+        if(!arguments.contains(arg)) {
+            arguments.plus(arg)
+        }
+    }
+}
+
+/**
+ * Represents a single argument on the command line.
+ */
+open class CLIArg {
+    /**
+     * Represents the `-h` or `--help` option.
+     */
+    class Help : CLIArg()
+
+    /**
+     * Represents the `-c` or `--code-client` option.
+     */
+    class CodeClient : CLIArg()
+
+    /**
+     * Represents the `-r` or `--recode` option.
+     */
+    class Recode : CLIArg()
+
+    /**
+     * Represents the `run` input.
+     */
+    class Run : CLIArg()
+
+    /**
+     * Represents the `--emit` option.
+     * @param emit Must be one of the following: `tokens`, `ast1`, `ast2`, `kindling`
+     */
+    class Emit(private val emit: String) : CLIArg()
+
+    /**
+     * Represents the `--verbose` option.
+     */
+    class Verbose : CLIArg()
 }
--- src/main/kotlin/dev/ashli/fire/Main.kt
+++ src/main/kotlin/dev/ashli/fire/Main.kt
@@ -5,39 +5,77 @@ import dev.ashli.fire.resources.ResourceName
 import dev.ashli.fire.tokenizer.tokenize
 
 fun main(args: Array<String>) {
-    if(args.isEmpty()) {
-        displayHelp()
-        return
-    }
-    when(args[0]) {
-        else -> {
-            displayHelp()
+    val arguments = CommandLineArguments();
+    val iter = args.iterator()
+    for(arg in iter) {
+        when(arg) {
+            "-h" -> {
+                displayHelp()
+                return
+            }
+            "-c" -> {
+                arguments.addArgument(CLIArg.CodeClient())
+            }
+            "-r" -> {
+                arguments.addArgument(CLIArg.Recode())
+            }
+            "--emit" -> {
+                val next = iter.next()
+                if(next == "tokens" || next == "ast1" || next == "ast2" || next == "kindling") {
+                    arguments.addArgument(CLIArg.Emit("next"))
+                } else {
+                    println("Error: Invalid emit target. Expected one of: `tokens`, `ast1`, `ast2`, `kindling`.")
+                    return
+                }
+            }
+            "--verbose" -> {
+                arguments.addArgument(CLIArg.Verbose())
+            }
+            "--test" -> {
+                when(iter.next()) {
+                    "token-test" -> {
+                        tokenTest()
+                    }
+                    else -> {
+                        println("Error: Invalid test. Expected one of: `token-test`")
+                    }
+                }
+                return
+            }
+            else -> {
+                println("Invalid option `$arg`!")
+                displayHelp()
+                return
+            }
         }
     }
 }
 
-// test
-// test 2
 fun displayHelp() {
     println("""
-Usage: fire [options] input
+Usage: java -jar fire.jar [options] [input]
 
 Options:
     -h, --help           Display this message
     -c, --code-client    Compile with CodeClient
     -r, --recode         Compile with Recode
-    --emit [tokens|ast1|ast2|kindling]  
+    --emit (tokens|ast1|ast2|kindling) 
                          Change what the compiler emits.
                          Use `tokens` to emit the tokenized version,
                          `ast1` to emit the first AST,
                          `ast2` to emit the second AST,
                          `kindling` to emit the raw Kindling.
     --verbose            Enable verbose output for debugging.
+    --test (token-test)  
+                         Run a testing function. (Temporary for developers)
+   
+Input:
+    run                  Run the current Fire directory. Reads from `fire/`
     
 """)
 
 }
-fun token_test() {
+fun tokenTest() {
     println(
         tokenize(
             """
